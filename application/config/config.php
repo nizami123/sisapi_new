28ab1d1083646345a4eabb89cc6d0b13d5d6ad63
Ahmad Alfian Nizami <ahmad.nizami@ambersof.com>
2026-08-22 10:45:28 +0700

    add new template

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/sisapi_rev/';
+$config['base_url'] = 'https://sisapi.siternal.my.id/';
 
 /*
 |--------------------------------------------------------------------------
